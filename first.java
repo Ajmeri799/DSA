f60f1315bbb1b3dee085a0cc55f8ccc71efa9816
Ajmeri-Guniya-14 <149288719+AjmeriGuniya@users.noreply.github.com>
2024-01-02 23:26:22 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,188 @@
+import java.util.Scanner;
+
+public class first {
+
+
+public static void main(String[]args){
+
+
+
+
+
+//    System.out.println("*");
+//    System.out.println("**");
+//    System.out.println("***");
+//    System.out.println("****");
+//    System.out.println("*****");
+//-----------------------------------------------------------------------------------------//
+//    int a,b ;
+//    Scanner sc=new Scanner(System.in);
+//    a = sc.nextInt();
+//    b= sc.nextInt();
+//     int c=(a*b)/(a-b);
+//    int d=a*b/a-b;
+//    System.out.println(c);
+//    System.out.println(d);
+
+//------------------------------------------------------------------------------------------------//
+
+//int button;
+//    System.out.println("enter the button num :");
+//    Scanner bt =new Scanner(System.in);
+//   button= bt.nextInt();
+//    switch (button){
+//        case 1:
+//            System.out.println("Hello");
+//            break;
+//        case 2:
+//            System.out.println("namaste");
+//            break;
+//        case 3:
+//            System.out.println("bonjao");
+//            break;
+//        default:
+//            System.out.println("invalid");
+//    }
+
+//-------------------------------------------------------------------------------------------------------//
+
+//    char a='*';
+//    for(int i=0; i<5;i++) {
+//        for (int j = 0; j < 5; j++) {
+//         //   System.out.print("\t" + a);
+//        }
+//        System.out.println();
+//    }
+//---------------------------------------------------------------------------------------------------------//
+
+//int n=4,m=5;
+//    for(int i=1; i<=4;i++) {
+//        for (int j = 1; j<=5; j++) {
+////
+////            if(i==1 ||j==m ||i==n || j==1) {
+////                System.out.print("*");
+////            }else {
+////                System.out.print(" ");
+////            }
+////        }
+////        System.out.println();
+//    }
+
+    //---------------------------------------------------------------------------------------------------------//
+//
+//int n=4;
+//for(int i=1; i<=n ;i++){
+//      for(int j=1; j<=i;j++){
+//          System.out.print("\t*");
+//      }
+//    System.out.println("");
+//}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//    int n=4;
+//    for(int i=0; i<=n ;i++){
+//        for(int j=1; j<=n-i;j++){
+//            System.out.print("\t*");
+//        }
+//        System.out.println("");
+//    }
+
+
+//---------------------------------------------------------------------------------------------------------------
+//    int n=5;
+//    for(int j=5; j>=0 ;j--){
+//        for(int i=1;i<=j;i++) {
+//            System.out.print("\t*");
+//        }
+////
+//////
+////
+//        System.out.println("");
+//    }
+
+
+    //----------------------------------------------------------------------------------------------
+//    int n=5;
+//    for(int j=5; j>=0 ;j--){
+//        for(int i=1;i<=j;i++) {
+//            System.out.print("\t ");
+//        }
+//        for(int z=1;z<=n-j;z++) {
+//            System.out.print("\t*");
+//        }
+//
+//        System.out.println("");
+//    }
+//-------------------------------------------------------------------
+//    int n=5;
+//    for(int j=1; j<=5 ;j++){
+//        for(int i=1;i<=j;i++) {
+//            System.out.print(j+"\t");
+//        }
+//
+//        System.out.println();
+//    }
+//---------------------------------------------------------------------------------------------------//
+//    for(int j=1; j<=5 ;j++){
+//        for(int i=1;i<=5-j+1;i++) {
+//            System.out.print(i+"\t");
+//        }
+//
+//        System.out.println();
+//
+//  }
+//---------------------------------------pattern_8----------------------------------------------------//
+//int num=1;
+//    for(int j=1; j<=5 ;j++){
+//        for(int i=1;i<=j;i++) {
+//            System.out.print(num+"\t");
+//            num++;
+//
+//        }
+//
+//        System.out.println();
+//
+//  }
+
+
+//-------------------------------------------------------------------------------------------------------------//
+
+//    int nun=1;
+//    for(int j=1; j<=5 ;j++){
+//        for(int i=1;i<=j;i++) {
+//            int sum=i+j;
+//     if(i!=j){
+//         System.out.print(nun+"\t");
+//     }
+//     else {
+//         System.out.print("0\t");
+//     }
+//        }
+
+  //      System.out.println();
+
+    //  }
+
+
+//--------------------------------------------------------------------------------------------------//
+
+//    int nun=1;
+//    for(int j=1; j<=5 ;j++){
+//        for(int i=1;i<=j;i++) {
+//            int sum=i+j;
+//    if(sum%2==0) {
+//        System.out.print(nun + "\t");
+////            num++;
+//    }
+//    else
+//    {
+//        System.out.print("0\t");
+//    }
+
+
+
+
+
+}
+}
