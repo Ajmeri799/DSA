9eced311e6a6d3b8e748937b886e38e2d4d20a13
Ajmeri-Guniya-14 <149288719+AjmeriGuniya@users.noreply.github.com>
2024-01-07 23:32:35 +0530

    Add files via upload

---
+++ DoubleLL.java
@@ -0,0 +1,20 @@
+public class DoubleLL {
+
+
+
+    public class Node {
+        String data;
+        Node next;
+        Node pre;
+
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+            this.pre=null;
+        }
+    }
+
+
+
+}
