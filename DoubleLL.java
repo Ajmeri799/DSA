ba362aa3ce9eb227fd53ea21a490577e76a23d79
Ajmeri-Guniya-14 <149288719+AjmeriGuniya@users.noreply.github.com>
2024-01-08 20:26:08 +0530

    Update DoubleLL.java

--- DoubleLL.java
+++ DoubleLL.java
@@ -23,5 +23,16 @@ public class DoubleLL {
               head=newNode;
         }
 
-
+       public void treverse(){
+            if(head==null){
+                System.out.println("List is empty");
+                return;
+            }
+            Node current=head;
+            while (current!=null){
+                System.out.print(current.data+"->");
+                current=current.next;
+            }
+            System.out.println("NULL");
+        }
 }
