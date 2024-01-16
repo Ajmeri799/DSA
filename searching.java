3ff82534296a67ba370dc93ed6db4a90b8ced66d
Ajmeri Gudiya <149288719+Ajmeri799@users.noreply.github.com>
2024-01-16 23:49:59 +0530

    Add files via upload

---
+++ searching.java
@@ -0,0 +1,23 @@
+public class searching {
+
+
+
+
+    public class Node {
+        String data;
+        circularLL.Node next;
+        circularLL.Node pre;
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+            this.pre=null;
+        }
+    }
+
+
+
+
+
+
+
+}
